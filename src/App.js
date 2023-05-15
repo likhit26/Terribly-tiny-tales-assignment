64cc9efd18e8412f03f198bd5e4bb7d0aacaf36e
Likhit Varma M <103757694+likhit26@users.noreply.github.com>
2023-05-16 03:20:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import _ from 'lodash';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import { saveAs } from 'file-saver';
 
-function App() {
+const App = () => {
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://www.terriblytinytales.com/test.txt');
+      const wordCounts = _.countBy(response.data.split(' '));
+      const sortedData = Object.entries(wordCounts)
+        .map(([word, count]) => ({ word, count }))
+        .sort((a, b) => b.count - a.count)
+        .slice(0, 20);
+      setData(sortedData);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  const handleExport = () => {
+    const csvData = data.map((item) => `${item.word},${item.count}`).join('\n');
+    const blob = new Blob([csvData], { type: 'text/csv;charset=utf-8' });
+    saveAs(blob, 'histogram.csv');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={fetchData}>Submit</button>
+      {data.length > 0 && (
+        <div>
+          <BarChart width={600} height={400} data={data}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="word" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="count" fill="#8884d8" />
+          </BarChart>
+          <button onClick={handleExport}>Export</button>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
+
